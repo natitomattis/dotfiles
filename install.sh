513226eea812471b8f27a614c6f69b51d63e717f
Nat Tomattis <natitomattis@gmail.com>
2025-01-03 16:50:06 -0300

    devbox

---
+++ install.sh
@@ -0,0 +1,17 @@
+brew install font-fira-code-nerd-font
+
+# https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#install
+bash -c "$(curl --fail --show-error --silent \
+    --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+# https://github.com/tonsky/FiraCode/wiki/Installing
+brew install --cask font-fira-code
+
+# https://www.jetify.com/devbox/docs/installing_devbox/
+curl -fsSL https://get.jetify.com/devbox | bash
+
+# https://github.com/hidetatz/kubecolor
+brew install kubecolor
+
+# https://github.com/sharkdp/bat
+brew install bat
